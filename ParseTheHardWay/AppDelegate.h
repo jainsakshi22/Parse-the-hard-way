72df145a29c086e0ab9ec62ab091c2dfaf8264c5
jainsakshi22 <jainsakshi22@gmail.com>
2015-03-16 12:38:41 +0530

    Initial Commit

---
+++ ParseTheHardWay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseTheHardWay
+//
+//  Created by Sakshi Jain on 16/03/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseTheHardWay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseTheHardWay
+//
+//  Created by Sakshi Jain on 16/03/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
